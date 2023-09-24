ef2470e621b5d0f1d7b155820d1f1065288e70d7
Aleksey Pekar <fenix_al@tut.by>
2023-09-24 19:57:23 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { configureStore } from './store';
 
-const store = configureStore();
+import {store} from './store';
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/store/details/details-selectors.js
+++
@@ -1,2 +0,0 @@
-export const selectCurrCountry = (state) => state.details;
-export const selectNeighbors = (state) => state.details.neighbors;
